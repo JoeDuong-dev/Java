ba2f3d913b0926ea111fe6d3e721dd5fc7b89299
Joe Duong <chau.duong1995@gmail.com>
2023-11-06 02:00:38 -0800

    Init

---
+++ CoreJavaBrushUp.java
@@ -0,0 +1,8 @@
+public class CoreJavaBrushUp {
+    public static void main(String[] args) {
+        int age = 28;
+        String name = "Joe";
+
+        System.out.println("My name is " + name + " and I am " + age);
+    }
+}
